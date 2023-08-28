729c38e5d7111e1b52d7fd4c57c71d36888a4725
Kresswell <130984721+Kresswell@users.noreply.github.com>
2023-08-28 13:53:33 +0300

    config.js

--- config.js
+++ config.js
@@ -6,13 +6,13 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
-global.email = 'sam@secktor.live'
-global.github = 'https://github.com/SamPandey001/Secktor-Md'
-global.location = 'Sultanpur IN'
-global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
-global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
+global.email = 'simonecresswell@outlook.com'
+global.github = 'https://github.com/Kresswell'
+global.location = 'Nairobi'
+global.gurl = 'https://instagram.com/Kresswell0' // add your username
+global.sudo = process.env.SUDO || '254768153167'
+global.devs = '254768153167';
+global.website = 'http://www.infinityhackers.xyz' //wa.me/+254768153167
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? 'AnonyMous42' : process.env.BOT_NAME,
